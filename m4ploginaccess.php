46a373ad261d8401f66db3963fb034650d4a68eb
Kamil <kamilekkrawczyk404@gmail.com>
2026-09-25 19:33:56 +0200

    chore: release under the MIT license
    Replace the all-rights-reserved headers with MIT and add LICENSE, composer.json,
    the config.xml/config_pl.xml manifests, index.php guards and the standard README,
    CONTRIBUTING, SECURITY and CHANGELOG files. Move strings to $this->trans() with
    the Admin and Shop domains and ship pl-PL and en-US catalogues. Declare PrestaShop
    compatibility, seed default settings on install, remove them on uninstall and drop
    an unused private method.

--- m4ploginaccess.php
+++ m4ploginaccess.php
@@ -1,16 +1,11 @@
 <?php
 
 /**
- * LICENCE
+ * m4ploginaccess
  *
- * ALL RIGHTS RESERVED.
- * YOU ARE NOT ALLOWED TO COPY/EDIT/SHARE/WHATEVER.
- *
- * IN CASE OF ANY PROBLEM CONTACT AUTHOR.
- *
- *  @author    Jan Kołodziej (contact@modules4presta.io)
- *  @copyright Modules4Presta.io
- *  @license   ALL RIGHTS RESERVED
+ * @author    Modules4Presta <contact@modules4presta.io>
+ * @copyright 2026 Nice Code sp. z o.o. (Modules4Presta)
+ * @license   https://opensource.org/licenses/MIT MIT License
  */
 
 if (!defined('_PS_VERSION_')) {
@@ -27,15 +22,19 @@ class M4pLoginAccess extends Module
         $this->author = 'Modules4Presta.io';
         $this->need_instance = 0;
         $this->bootstrap = true;
+        $this->ps_versions_compliancy = ['min' => '1.7.6.0', 'max' => _PS_VERSION_];
 
         parent::__construct();
 
-        $this->displayName = $this->l('Login Access Control');
-        $this->description = $this->l('Enforce login and block possible to buy products for guests.');
+        $this->displayName = $this->trans('Login Access Control', [], 'Modules.M4ploginaccess.Admin');
+        $this->description = $this->trans('Enforce login and block possible to buy products for guests.', [], 'Modules.M4ploginaccess.Admin');
     }
 
     public function install()
     {
+        Configuration::updateValue('M4PLOGINACCESS_FORCE_LOGIN', 0);
+        Configuration::updateValue('M4PLOGINACCESS_HIDE_PRICES', 0);
+
         return parent::install()
             && $this->registerHook('displayHeader')
             && $this->registerHook('displayProductPriceBlock');
@@ -43,6 +42,9 @@ class M4pLoginAccess extends Module
 
     public function uninstall()
     {
+        Configuration::deleteByName('M4PLOGINACCESS_FORCE_LOGIN');
+        Configuration::deleteByName('M4PLOGINACCESS_HIDE_PRICES');
+
         return parent::uninstall();
     }
 
@@ -57,7 +59,7 @@ class M4pLoginAccess extends Module
             Configuration::updateValue('M4PLOGINACCESS_FORCE_LOGIN', (int) $force_login);
             Configuration::updateValue('M4PLOGINACCESS_HIDE_PRICES', (int) $hide_prices);
 
-            $output .= $this->displayConfirmation($this->l('Settings updated'));
+            $output .= $this->displayConfirmation($this->trans('Settings updated', [], 'Modules.M4ploginaccess.Admin'));
         }
 
         return $output.$this->renderForm();
@@ -68,49 +70,49 @@ class M4pLoginAccess extends Module
         $fields_form = array(
             'form' => array(
                 'legend' => array(
-                    'title' => $this->l('Settings'),
+                    'title' => $this->trans('Settings', [], 'Modules.M4ploginaccess.Admin'),
                     'icon' => 'icon-cogs'
                 ),
                 'input' => array(
                     array(
                         'type' => 'switch',
-                        'label' => $this->l('Enforce login site-wide'),
+                        'label' => $this->trans('Enforce login site-wide', [], 'Modules.M4ploginaccess.Admin'),
                         'name' => 'M4PLOGINACCESS_FORCE_LOGIN',
                         'is_bool' => true,
                         'values' => array(
                             array(
                                 'id' => 'active_on',
                                 'value' => 1,
-                                'label' => $this->l('Enabled')
+                                'label' => $this->trans('Enabled', [], 'Modules.M4ploginaccess.Admin')
                             ),
                             array(
                                 'id' => 'active_off',
                                 'value' => 0,
-                                'label' => $this->l('Disabled')
+                                'label' => $this->trans('Disabled', [], 'Modules.M4ploginaccess.Admin')
                             )
                         )
                     ),
                     array(
                         'type' => 'switch',
-                        'label' => $this->l('Block possible to buy products and see prices for guests'),
+                        'label' => $this->trans('Block possible to buy products and see prices for guests', [], 'Modules.M4ploginaccess.Admin'),
                         'name' => 'M4PLOGINACCESS_HIDE_PRICES',
                         'is_bool' => true,
                         'values' => array(
                             array(
                                 'id' => 'active_on',
                                 'value' => 1,
-                                'label' => $this->l('Enabled')
+                                'label' => $this->trans('Enabled', [], 'Modules.M4ploginaccess.Admin')
                             ),
                             array(
                                 'id' => 'active_off',
                                 'value' => 0,
-                                'label' => $this->l('Disabled')
+                                'label' => $this->trans('Disabled', [], 'Modules.M4ploginaccess.Admin')
                             )
                         )
                     ),
                 ),
                 'submit' => array(
-                    'title' => $this->l('Save'),
+                    'title' => $this->trans('Save', [], 'Modules.M4ploginaccess.Admin'),
                 ),
             ),
         );
@@ -139,24 +141,10 @@ class M4pLoginAccess extends Module
             && empty($this->context->customer->isLogged())
             && $params['type'] == 'price'
         ) {
-            return '<p class="m4ploginaccess-note">'.$this->l('Please log in to buy products and see prices.').'</p>';
+            return '<p class="m4ploginaccess-note">'.$this->trans('Please log in to buy products and see prices.', [], 'Modules.M4ploginaccess.Shop').'</p>';
         }
     }
 
-    private function checkIfCustomerIsOnAccessPages()
-    {
-        $basePath = Tools::getHttpHost(true);
-
-        $uri = $_SERVER['REQUEST_URI'];
-        $path = parse_url($uri, PHP_URL_PATH);
-
-        $relative = $path;
-        if (strpos($path, $basePath) === 0) {
-            $relative = substr($path, strlen($basePath));
-        }
-
-        $relative = ltrim($relative, '/');
-    }
 
     public function hookDisplayHeader()
     {
